9e710500a4ec1df6f069e79ca8fe399f5108e844
TerSom <ibnua0291@gmail.com>
2025-11-14 20:13:42 +0700

    belajar express

--- Backend/app.js
+++ Backend/app.js
@@ -13,10 +13,10 @@ let users = [
 ]
 
 let books = [
-    {
+     {
         id : 1,
-        buku : "Perjuangan",
-        stok : 2
+        buku : "perjuanga",
+        stok : 1
     },
 ]
 
@@ -28,9 +28,12 @@ app.get('/book' , (req,res) => {
 // TAMBAH BUKU
 app.post('/book/tambah' , (req,res) => {
     const {buku} = req.body
+    const {stok} = req.body
+
     books.push({
         id : books.length + 1,
-        buku
+        buku,
+        stok
     })
 
     res.status(201).json({
@@ -41,7 +44,7 @@ app.post('/book/tambah' , (req,res) => {
 
 // DELETE BUKU
 app.delete('/book/:id/delete' , (req,res) => {
-    const bookId = req.params.id
+    const bookId = parseInt (req.params.id)
     const iBook = books.findIndex(b => b.id === bookId)
 
     books.splice(iBook, 1)
@@ -53,21 +56,23 @@ app.delete('/book/:id/delete' , (req,res) => {
 
 // EDIT BUKU
 app.put("/book/:id/edit" , (req,res) => {
-    const id = req.params.id
+    const bookId = parseInt(req.params.id)
     const {buku} = req.body
     const {stok} = req.body
-    const iBook = books.findIndex(b => b.id == id)
+    const iBook = books.findIndex(b => b.id == bookId)
 
     books[iBook] = {
-        id : parseInt(id),
+        id : parseInt(iBook + 1),
         buku,
         stok
     }
 
     res.status(200).json({
-        msg : `id : ${id} update`
+        msg : `id : ${iBook} update`
     })
 })
+
+// =======================================================================================================
 
 // USER
 app.get('/user' , (req,res) => {
@@ -85,15 +90,15 @@ app.post('/user/tambah', (req,res) => {
     })
 
     res.status(201).json({
-        msg : "user sudah di tambahkan"
+        msg : `${nama} sudah di tambahkan`
 
     })
 })
 
 // DELETE USER
 app.delete('/user/:id/delete', (req,res) => {
-    const id = req.params.id
-    const iUser = users.findIndex(u => u.id == id)
+    const userId = parseInt(req.params.id)
+    const iUser = users.findIndex(u => u.id == userId)
 
     users.splice(iUser,1)
 
@@ -123,15 +128,15 @@ app.put('/user/:id/pinjam', (req,res) => {
         })  
     }
 
-    book.stok -= 1
+    if(book.stok <= 0){
+        books.splice(iBook, 1)
+    }
 
+    book.stok -= 1
     users[iUser].BukuPinjaman.push({
         buku : book.buku
     })
 
-    if(book.stok <= 0){
-        books.splice(iBook, 1)
-    }
 
     res.status(200).json({
         msg : "Buku berhasil di pinjam",
@@ -142,7 +147,7 @@ app.put('/user/:id/pinjam', (req,res) => {
     })
 })
 
-// BALIKIN BUKU
+// USER BALIKIN BUKU
 app.put('/user/:id/balikin', (req,res) => {
     const userId = parseInt(req.params.id)
     const {bukuNama} = req.body 
@@ -151,17 +156,24 @@ app.put('/user/:id/balikin', (req,res) => {
     const user = users[iUser]
     const book = books[iBook]
 
+    if(!book){
+        return res.status(404).json({
+            msg : `tidak ada buku yang namanya ${bukuNama}`
+        })  
+    }
+
+    if(!user){
+        return res.status(404).json({
+            msg : `tidak ada user`
+        })  
+    }
+
     if(users[iUser].BukuPinjaman.length <= 0 ){
         return res.status(404).json({
             msg : "buku tidak ada"
         })  
     }
 
-    if(!book){
-        return res.status(404).json({
-            msg : `tidak ada buku yang namanya ${bukuNama}`
-        })  
-    }
     
     book.stok += 1
     users[iUser].BukuPinjaman.splice(iBook, 1)
